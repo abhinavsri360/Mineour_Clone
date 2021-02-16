19aea6752de2ffc8e51475606f1b974a5e5537b6
abhinavsri360 <abhinav.022000@gmail.com>
2021-02-16 19:42:29 +0530

    Auth, Firebase done

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Login from './Login'
+import Signup from './Signup'
+import Dashboard from './Dashboard'
+import PrivateRoute from './PrivateRoute'
+import UpdateProfile from './UpdateProfile'
+import ForgotPassword from './ForgotPassword'
+import { Container } from 'react-bootstrap'
+import { AuthProvider } from '../contexts/AuthContext'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+
+function App () {
+  return (
+    <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
+      <div className='w-100' style={{ maxWidth: '400px' }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path='/' component={Dashboard} />
+              <PrivateRoute path='/update-profile' component={UpdateProfile} />
+              <Route path='/signup' component={Signup} />
+              <Route path='/login' component={Login} />
+              <Route path='/forgot-password' component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+  )
+}
+
+export default App
