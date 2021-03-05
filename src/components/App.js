2e915194e2f52f769a440708072203c1f6e7e7f0
samriddhisrivastava <55881704+samriddhisrivastava@users.noreply.github.com>
2021-03-05 23:07:53 +0530

    added background image for app

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,20 @@ import ForgotPassword from './ForgotPassword'
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from '../contexts/AuthContext'
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import background from "../LoginScreen.jpg";
+
 
 function App () {
+
+  const container_styling = {
+    minHeight: '100vh',
+    minWidth: '100%',
+    backgroundImage: `url(${background})` 
+  };
+
+
   return (
-    <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
+    <Container className='d-flex align-items-center justify-content-center' style={container_styling}>
       <div className='w-100' style={{ maxWidth: '400px' }}>
         <Router>
           <AuthProvider>
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,8 @@ import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 import { Link, useHistory } from 'react-router-dom'
 
+import { Container } from 'react-bootstrap'
+
 export default function Login () {
   const emailRef = useRef()
   const passwordRef = useRef()
@@ -26,33 +28,43 @@ export default function Login () {
     setLoading(false)
   }
 
+  
+  
+  const card_styling = {
+    backgroundColor: 'white',
+    color: 'Black',
+    borderColor: 'white'
+
+  };
+
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className='text-center mb-4'>Log In</h2>
-          {error && <Alert variant='danger'>{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id='email'>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type='email' ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id='password'>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type='password' ref={passwordRef} required />
-            </Form.Group>
-            <Button disabled={loading} className='w-100' type='submit'>
-              Log In
-            </Button>
-          </Form>
-          <div className='w-100 text-center mt-3'>
-            <Link to='/forgot-password'>Forgot Password?</Link>
-          </div>
-        </Card.Body>
-      </Card>
-      <div className='w-100 text-center mt-2'>
-        Need an account? <Link to='/signup'>Sign Up</Link>
-      </div>
+        <Card style={card_styling}>
+          <Card.Body>
+            <h2 className='text-center mb-4'>Log In</h2>
+            {error && <Alert variant='danger'>{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id='email'>
+                <Form.Label>Email</Form.Label>
+                <Form.Control type='email' ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id='password'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control type='password' ref={passwordRef} required />
+              </Form.Group>
+              <Button disabled={loading} className='w-100' type='submit'>
+                Log In
+              </Button>
+            </Form>
+            <div className='w-100 text-center mt-3'>
+              <Link to='/forgot-password'>Forgot Password?</Link>
+            </div>
+          </Card.Body>
+        </Card>
+        <div className='w-100 text-center mt-2'>
+          Need an account? <Link to='/signup'>Sign Up</Link>
+        </div>
     </>
   )
 }
+
